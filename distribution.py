1aab8587e6eb275a68d1d9e3857442690c9031a4
dihimono <dihimono@gmail.com>
2017-09-09 20:04:26 +0800

    Update distribution.py
    Change it into 8-directional

--- distribution.py
+++ distribution.py
@@ -4,11 +4,8 @@ import sys
 file_name = sys.argv[1]
 
 # init four direction count
-up = 0
-right = 0
-down = 0
-left = 0
 count = 0
+area = [0] * 8
 
 with open(file_name, 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -16,22 +13,11 @@ with open(file_name, 'rb') as csvfile:
         angle = float(row[2])
         #print angle
         count = count + 1
-        if angle < 135 and angle > 45:
-            up = up + 1
-            #print "up"
-        elif angle < 225.0 and angle > 135.0:
-            left = left + 1
-            #print "left"
-        elif angle < 315.0 and angle > 225.0:
-            down = down + 1
-            #print "down"
-        elif angle < 45.0 or angle > 315.0:
-            right = right + 1
-            #print "right"
-        else :
-            print ("exception")
-print "up: ", up, " ", float(up)/count * 100, "%" 
-print "right: ", right, " ", float(right)/count * 100, "%"
-print "down: ", down, " ", float(down)/count * 100, "%"
-print "left ", left, " ", float(left)/count * 100, "%"
-     
+        for counter in xrange(8):
+          l = counter * 45 
+          r = (counter + 1) * 45
+          if angle >= l and angle < r:
+            area[counter] = area[counter] + 1
+for i in xrange(8):
+  print "Area ", i, " ", area[i], " ", 100.0 * area[i] / count, "%"
+print "total: ", count
