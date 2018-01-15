7d7e09172e5388a369b2149b7896b9ac10e94c5d
信丞 侯 <dihimono@Mac-mini.local>
2018-01-15 11:56:01 +0800

    Change some text

--- Study2/react/pilot/src/Forms.js
+++ Study2/react/pilot/src/Forms.js
@@ -63,8 +63,8 @@ class Forms extends Component {
                     case 1:
                         return (
                             <div> 
-                                    你好，我們是台大資工所行動與人機互動實驗室(NTU mobile HCI lab)。這份問卷主要是為了調查大家對於對話情境中 AR 文字擺放位置的偏好。整份問卷填寫時間大約 10 分鐘。
-                                    接下來，你將會看到很多段的影片。每段影片中，請評估自己若與影片中的人對話時，對於影片中文字方塊出現的位置的喜好度，以 1 ~ 5 分來評分。
+                                    你好，我們是台大資工所行動與人機互動實驗室(NTU mobile HCI lab)。這份問卷主要是為了調查大家對於對話情境中 AR 文字擺放位置的偏好。整份問卷填寫時間大約 20 分鐘。
+                                    接下來，你將會看到很多段的影片。每段影片上會有一個色塊，請評估自己若與影片中的人對話時，對於在色塊中擺放文字並閱讀的喜好程度，以 1 ~ 5 分來評分。
                             </div>
                         );
                     case 23:
@@ -99,7 +99,7 @@ class Forms extends Component {
                         return (
                             <div>
                                 <div>
-                                        請依照自己在這個方框內閱讀文字的意願喜好程度來進行評分，一分為最不願意，五分為最願意    
+                                        請依照自己在這個色塊內閱讀文字的意願喜好程度來進行評分，一分為最不願意，五分為最願意    
                                 </div>   
                                 <input type="radio" className="_radio" name="groupOptions" onClick={() => this.myclickRadio(5)}/> 5
                                 <input type="radio" className="_radio" name="groupOptions" onClick={() => this.myclickRadio(4)}/> 4
